0433b608d093dba1beece6ee98c0294509de36ae
jykhor <jing.yong.khor@intel.com>
2022-08-23 09:41:29 +0800

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+port = int(os.getenv('PORT', '3000'))
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port)
